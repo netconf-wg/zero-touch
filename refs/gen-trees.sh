a3d131b5675d7527d3be6952fe8a5a39da942e45
Kent Watsen <kent@watsen.net>
2016-07-08 12:22:32 -0400

    this is -09, again

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,2 +1,2 @@
 pyang -f tree ../ietf-zerotouch-bootstrap-server\@*.yang > ietf-zerotouch-bootstrap-server-tree.txt 
-pyang -f tree ../ietf-zerotouch-ownership-voucher\@*.yang > ietf-zerotouch-ownership-voucher-tree.txt 
+pyang -f tree ../example-zerotouch-ownership-voucher\@*.yang > example-zerotouch-ownership-voucher-tree.txt 
