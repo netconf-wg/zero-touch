2e7564312c1a0927aabfbeff99e5672d4abc9abc
Kent Watsen <kent@watsen.net>
2017-08-15 20:23:53 -0400

    this is -15

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,3 +1,3 @@
-pyang -f tree --tree-print-yang-data ../ietf-zerotouch-information\@*.yang | sed -e '2,3 d' -e 's/^    //' > ietf-zerotouch-information-tree.txt 
+pyang -f tree --tree-print-yang-data ../ietf-zerotouch-information\@*.yang > ietf-zerotouch-information-tree.txt 
 
 pyang -f tree ../ietf-zerotouch-bootstrap-server\@*.yang > ietf-zerotouch-bootstrap-server-tree.txt 
--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -1,12 +1,23 @@
-pyang --ietf --strict ../ietf-zerotouch\@*.yang
+echo "Testing ietf-keystore.yang..."
+pyang --ietf --strict --max-line-length=70 ../ietf-zerotouch-bootstrap-server\@*.yang
+pyang --ietf --strict --max-line-length=70 ../ietf-zerotouch-information\@*.yang
+pyang --canonical ../ietf-zerotouch-bootstrap-server\@*.yang
+pyang --canonical ../ietf-zerotouch-information\@*.yang
+yanglint ../ietf-zerotouch-bootstrap-server\@*.yang
+yanglint ../ietf-zerotouch-information\@*.yang
 
-./validate.sh ietf-zerotouch\@*.yang ex-file-redirect-information.xml
-./validate.sh ietf-zerotouch\@*.yang ex-file-bootstrap-information.xml
-./validate.sh ietf-zerotouch\@*.yang ex-file-owner-certificate.xml
-./validate.sh ietf-zerotouch\@*.yang ex-file-ownership-voucher.xml
+echo "Testing ex-api-redirect-information-unsigned..."
+yanglint -s ../ietf-zerotouch-bootstrap-server\@*.yang ex-api-redirect-information-unsigned.xml
 
-./validate.sh ietf-zerotouch\@*.yang ex-api-redirect-information-signed.xml
-./validate.sh ietf-zerotouch\@*.yang ex-api-bootstrap-information-signed.xml
-./validate.sh ietf-zerotouch\@*.yang ex-api-redirect-information-unsigned.xml
-./validate.sh ietf-zerotouch\@*.yang ex-api-bootstrap-information-unsigned.xml
+echo "Testing ex-api-redirect-information-signed..."
+yanglint -s ../ietf-zerotouch-bootstrap-server\@*.yang ex-api-redirect-information-signed.xml
+
+echo "Testing ex-api-bootstrap-complete-notification..."
+yanglint -s -t auto ../ietf-zerotouch-bootstrap-server\@*.yang ex-api-bootstrap-complete-notification.xml
+
+echo "Testing ex-file-redirect-information..."
+yanglint -s ../ietf-zerotouch-information\@*.yang ex-file-redirect-information.xml
+
+echo "Testing ex-file-onboarding-information..."
+yanglint -s ../ietf-zerotouch-information\@*.yang ex-file-onboarding-information.xml
 
