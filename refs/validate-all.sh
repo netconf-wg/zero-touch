8f8f4ca70fb68c687fd0f8530b6c45ccb63a4d25
Kent Watsen <kent@watsen.net>
2017-08-25 16:04:27 -0400

    clean up

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -1,23 +1,25 @@
-echo "Testing ietf-keystore.yang..."
+echo "Testing ietf-zerotouch-bootstrap-server.yang..."
 pyang --ietf --strict --max-line-length=70 ../ietf-zerotouch-bootstrap-server\@*.yang
-pyang --ietf --strict --max-line-length=70 ../ietf-zerotouch-information\@*.yang
 pyang --canonical ../ietf-zerotouch-bootstrap-server\@*.yang
-pyang --canonical ../ietf-zerotouch-information\@*.yang
 yanglint ../ietf-zerotouch-bootstrap-server\@*.yang
+
+echo "Testing ietf-zerotouch--information.yang..."
+pyang --ietf --strict --max-line-length=70 ../ietf-zerotouch-information\@*.yang
+pyang --canonical ../ietf-zerotouch-information\@*.yang
 yanglint ../ietf-zerotouch-information\@*.yang
 
-echo "Testing ex-api-redirect-information-unsigned..."
+echo "Testing ex-api-redirect-information-unsigned.xml..."
 yanglint -s ../ietf-zerotouch-bootstrap-server\@*.yang ex-api-redirect-information-unsigned.xml
 
-echo "Testing ex-api-redirect-information-signed..."
+echo "Testing ex-api-redirect-information-signed.xml..."
 yanglint -s ../ietf-zerotouch-bootstrap-server\@*.yang ex-api-redirect-information-signed.xml
 
-echo "Testing ex-api-bootstrap-complete-notification..."
+echo "Testing ex-api-bootstrap-complete-notification.xml..."
 yanglint -s -t auto ../ietf-zerotouch-bootstrap-server\@*.yang ex-api-bootstrap-complete-notification.xml
 
-echo "Testing ex-file-redirect-information..."
+echo "Testing ex-file-redirect-information.xml..."
 yanglint -s ../ietf-zerotouch-information\@*.yang ex-file-redirect-information.xml
 
-echo "Testing ex-file-onboarding-information..."
+echo "Testing ex-file-onboarding-information.xml..."
 yanglint -s ../ietf-zerotouch-information\@*.yang ex-file-onboarding-information.xml
 
