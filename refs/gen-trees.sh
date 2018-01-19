4bf4322a26ac0d87890d73e29e2cce9b7f57b517
Kent Watsen <kent@watsen.net>
2018-01-19 11:45:52 -0500

    combination of -19 (ooops) and pre-20

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,5 +1,9 @@
 pyang -f tree --tree-print-yang-data ../ietf-zerotouch-information\@*.yang > ietf-zerotouch-information-tree.txt 
 
+cat ietf-zerotouch-information-tree.txt | sed -n '/:(redirect-information)/,/:(onboarding-information)/p' | sed '$d' | sed 's/^         |/          /' | sed 's/         //' > redirect-information-tree.txt 
+
+cat ietf-zerotouch-information-tree.txt | sed -n '/:(onboarding-information)/,$p' | sed 's/         //'  > onboarding-information-tree.txt
+
 pyang -f tree ../ietf-zerotouch-bootstrap-server\@*.yang > ietf-zerotouch-bootstrap-server-tree.txt 
 
 pyang -p ../ -f tree --tree-line-length=70 ../ietf-zerotouch-device\@*.yang > ietf-zerotouch-device-tree.txt 
